c0167e283f40f9efe76fd1102f260e2f0b4aba36
Leenye <liyujing069@163.com>
2015-07-12 15:05:41 +0800

    finish hibernate configuration

--- core/src/main/java/com/tw/Dao/UsrDao.java
+++ core/src/main/java/com/tw/Dao/UsrDao.java
@@ -1,5 +1,11 @@
 package com.tw.dao;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.Query;
+
+
 import com.tw.entity.Usr;
 
 import java.sql.*;
---
+++ core/src/main/java/com/tw/util/HibernateUtil.java
@@ -0,0 +1,19 @@
+package com.tw.util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil{
+    private static SessionFactory sessionFactory ;
+    static {
+        Configuration configuration = new Configuration().configure();
+        StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
+        sessionFactory = configuration.buildSessionFactory(builder.build());
+    }//在其他代码执行之前会先被调用，而且只调用一次（单例模式）
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+}
+
